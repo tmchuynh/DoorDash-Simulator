661c0d1ceefc5ca5603cda27872a371cde45c10d
Tina Huynh <tinamchuynh@gmail.com>
2023-02-20 12:53:25 -0800

    get prices

--- public/delivery.js
+++ public/delivery.js
@@ -22,4 +22,34 @@ function getFormValues() {
     return payload;
 }
 
-getFormValues();
+async function getFee() {
+    const paylod = getFormValues();
+
+    const finalPayload = JSON.stringify(paylod);
+
+    const formInput = document.querySelector("form");
+
+    if (formInput.checkValidity()) {
+        const response = await fetch("/get-delivery-rate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: finalPayload
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+        const deliveryFee = document.getElementById("fee");
+        const clothingTotal = document.getElementById("price");
+        const orderTotal = document.getElementById("total");
+
+        clothingTotal.textContent = `$${(window.menuItems / 100).toFixed(2)}`;
+        deliveryFee.textContent = `$${(response.data.fee / 100).toFixed(2)}`;
+        orderTotal.textContent = `$${((Number(window.menuItems) + response.data.fee) / 100).toFixed(2)}`;
+    }
+}
